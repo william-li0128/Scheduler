bf5ee84ecdef1c5c2ac86783c6b0bb22721a6979
William Li <110069721+william-li0128@users.noreply.github.com>
2023-02-11 18:18:35 -0800

    Setup Appointment/index.js

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,3 +135,14 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => (
+    <Appointment 
+      time={"12pm"}
+    />
+  ))
