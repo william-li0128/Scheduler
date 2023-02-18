aad30c2a9866844bf33d164708641c5160455826
William Li <110069721+william-li0128@users.noreply.github.com>
2023-02-18 14:33:15 -0800

    build getAppointmentsForDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+export function getAppointmentsForDay(state, day) {
+  let resultArray = [];
+
+  for (const dayObject of state.days) {
+    if (day === dayObject.name) {
+      resultArray = dayObject.appointments.map(id => state.appointments[id]);
+    }
+  }
+
+  return resultArray;
+}
