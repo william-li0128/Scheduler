5ee61a00835f626d9af48d4bbb376cc50f88db05
William Li <110069721+william-li0128@users.noreply.github.com>
2023-02-19 12:22:48 -0800

    finish useVisualMode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,33 @@
 import React from "react";
 
+import useVisualMode from "hooks/useVisualMode";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
 import "components/Appointment/styles.scss"
 
-
-
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header
         time={props.time}
       />
-      {props.interview ? 
-      <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : 
-      <Empty/>}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    history.push(mode)
+    setMode(mode);
+    setHistory(history);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setHistory(history);
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
