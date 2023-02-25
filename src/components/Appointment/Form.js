f2936ba249c0a8725a9cbbd77262ea5b35158404
William Li <110069721+william-li0128@users.noreply.github.com>
2023-02-25 00:07:42 -0800

    validate form input on submit

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setStudent("");
@@ -15,6 +16,20 @@ export default function Form(props) {
   const cancel =function() {
     reset();
     props.onCancel();
+  }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
   }
 
   return (
@@ -28,8 +43,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
             value={student}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers}
           value={interviewer}
@@ -39,7 +56,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={event => validate()}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,59 +1,10 @@
-/*
-  We are rendering `<Appointment />` down below, so we need React.createElement
-*/
 import React from "react";
+import { render} from "@testing-library/react";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
-import { render } from "@testing-library/react";
-
-/*
-  We import the component that we are testing
-*/
 import Appointment from "components/Appointment/index";
-import Confirm from "components/Appointment/Confirm";
-import Empty from "components/Appointment/Empty";
-import Error from "components/Appointment/Error";
-import Form from "components/Appointment/Form";
-import Header from "components/Appointment/Header";
-import Show from "components/Appointment/Show";
-import Status from "components/Appointment/Status";
 
-/*
-  A test that renders a React Component
-*/
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
-  });
-
-  it("renders Confirm component without crashing", () => {
-    render(<Confirm />);
-  });
-
-  it("renders Empty component without crashing", () => {
-    render(<Empty />);
-  });
-
-  it("renders Error component without crashing", () => {
-    render(<Error />);
-  });
-
-  it("renders Form component without crashing", () => {
-    render(<Form />);
-  });
-
-  it("renders Header component without crashing", () => {
-    render(<Header />);
-  });
-
-  it("renders Show component without crashing", () => {
-    render(<Show />);
-  });
-
-  it("renders Status component without crashing", () => {
-    render(<Status />);
   });
 });
