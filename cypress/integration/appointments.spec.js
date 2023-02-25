7c3bf644f8121c4a7b17af40a75c07531be670ac
William Li <110069721+william-li0128@users.noreply.github.com>
2023-02-25 10:08:10 -0800

    write 3 cy tests for appointments

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,65 @@
+describe("Appointments", () => {
+
+  beforeEach(() => {
+    // Reset database before testing
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({force: true});//pass { force: true } to the button to bypass the hover act
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({force: true});//pass { force: true } to the button to bypass the hover act
+
+    cy.contains("Confirm")
+      .click();
+
+    //check that the "Deleting" indicator should exist
+    cy.contains("Deleting").should("exist");
+    //Then check that the "Deleting" indicator should not exist.
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
+});
